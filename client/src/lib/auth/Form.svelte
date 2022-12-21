2654da9fa5e5735b1a012f0c308269ccb6d0443d
dylan <dylanc.codcazeraz>
2022-12-21 18:31:40 +0100

    :hammer: Make form's html structure

---
+++ client/src/lib/auth/Form.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let signUp;
+
+  const [formValues, errors] = [{}, {}];
+
+  const handleSubmit = () => {};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label for="email">Email</label><br />
+  <input type="email" id="email" bind:value={formValues.email} /><br />
+
+  {#if signUp}
+    <label for="name">Full name</label>
+    <input type="text" id="name" bind:value={formValues.name} />
+  {/if}
+
+  <label for="password">Password</label>
+  <input type="password" id="password" bind:value={formValues.password} />
+
+  {#if signUp}
+    <label for="confirm">Confirm password</label>
+    <input type="password" id="confirm" />
+
+    <input type="checkbox" id="cgv" bind:checked={formValues.checked} />
+    <label for="cgv">Please accept the CGV to continue.</label>
+
+    <button disabled={!formValues.checked}>Sign Up</button>
+  {:else}
+    <button disabled={!formValues.checked}>Sign In</button>
+  {/if}
+</form>
---
+++ client/src/routes/auth/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Form from "../../lib/auth/Form.svelte";
+
+  let signUp = true;
+</script>
+
+{#if signUp}
+  <h1>Create your account</h1>
+{:else}
+  <h1>Sign in</h1>
+{/if}
+
+<Form {signUp} />
