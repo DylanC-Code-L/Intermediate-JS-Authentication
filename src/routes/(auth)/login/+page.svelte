baf467b6da7436213d5c6fc3d3e6f383724fa646
dylan <dylanc.codcazeraz>
2022-12-29 11:49:50 +0100

    Add global error to register form

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import Email from "$lib/components/auth/Email.svelte";
   import Password from "$lib/components/auth/Password.svelte";
+  import Error from "$lib/components/Error.svelte";
 
   export let form;
 </script>
 
 <form method="post" class="flex flex-col">
+  <Error error={form?.errors?.global} />
   <Email value={form?.email} error={form?.errors?.email} />
   <Password error={form?.errors?.password} />
 
